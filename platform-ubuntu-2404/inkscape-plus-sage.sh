d7d6c66ffa3324020f4509da1c6ed5859787dc76
Ricardo Olsen <olsen.ricardo@gmail.com>
2026-04-23 10:26:55 +0200

    Update custom Inkscape compilation on Ubuntu platform.

--- platform-ubuntu-2404/inkscape-plus-sage.sh
+++ platform-ubuntu-2404/inkscape-plus-sage.sh
@@ -7,10 +7,23 @@ JS_USERNAME=jsonscada
 sudo apt remove -y inkscape
 # Inkscape build dependencies
 sudo apt -y install ninja-build libjpeg-dev libxslt-dev libgtkmm-3.0-dev libboost-all-dev \
-    libpoppler-dev libpoppler-glib-dev libgtest-dev libharfbuzz-dev libwpg-dev librevenge-dev libvisio-dev \
+    libgtest-dev libharfbuzz-dev libwpg-dev librevenge-dev libvisio-dev \
     libcdr-dev libreadline-dev libmagick++-dev libgraphicsmagick++1-dev libpango1.0-dev libgsl-dev \
     libsoup2.4-dev liblcms2-dev libgc-dev libdouble-conversion-dev potrace python3-scour lib2geom-dev
-sudo apt -y install libgspell-1-dev libgspell-1-2 libpotrace-dev libpoppler-private-dev
+sudo apt -y install libgspell-1-dev libgspell-1-2 libpotrace-dev libnss3-dev libgpgmepp-dev libcurlpp-dev
+
+cd /home/jsonscada
+
+# install poppler dependencies from apt
+sudo apt -y install libpoppler-dev libpoppler-glib-dev libpoppler-private-dev
+# or install poppler from source
+#  sudo -u $JS_USERNAME sh -c 'wget -v https://poppler.freedesktop.org/poppler-26.04.0.tar.xz -O poppler.tar.xz'
+#  sudo -u $JS_USERNAME sh -c 'tar -xf poppler.tar.xz'
+#  cd poppler-26.04.0
+#  sudo -u $JS_USERNAME sh -c 'mkdir build'
+#  cd build
+#  sudo -u $JS_USERNAME sh -c 'cmake .. -DENABLE_GPGME=OFF -DENABLE_QT6=OFF  -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=release'
+#  sudo make install
 
 cd /home/jsonscada
 sudo -u $JS_USERNAME sh -c 'git clone https://gitlab.com/ricardolo/inkscape-scadavis-editor.git'
@@ -28,10 +41,11 @@ cd build
 # to compile on Windows with msys2, use -DCMAKE_CXX_STANDARD=20
 # to compile on Linux, use -DCMAKE_CXX_STANDARD=17
 
+# use -DENABLE_POPPLER=OFF to disable poppler (pdf import) that can cause compilation issues
 #sudo -u $JS_USERNAME sh -c 'cmake -DENABLE_POPPLER_CAIRO=OFF -DCMAKE_CXX_STANDARD=17 ..'
 #sudo -u $JS_USERNAME sh -c 'make'
 #sudo make install
 sudo -u $JS_USERNAME sh -c 'cmake -DCMAKE_INSTALL_PREFIX="${PWD}/install_dir" -DENABLE_POPPLER=OFF -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_BUILD_TYPE=Release -DWITH_INTERNAL_2GEOM=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja ..'
 sudo -u $JS_USERNAME sh -c 'ninja -j4'
 sudo -u $JS_USERNAME sh -c 'ninja -j4'
-sudo ninja install
+sudo -u $JS_USERNAME sh -c 'ninja install'
